798deb2ca00b197fb2c202c57832ab6040da07f3
Jennifer <jeeniifer@hotmail.se>
2020-12-02 20:41:20 +0100

    server fix

--- Server/index.js
+++ Server/index.js
@@ -1,23 +1,24 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-
-import usersRoutes from '../Server/router.js';
+import express from "express";
+import bodyParser from "body-parser";
+import usersRoutes from "../Server/router.js";
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.use('/router', usersRoutes);
+app.use("/router", usersRoutes);
 
-/*app.get('/', (req, res) => {
-   res.send('Hello from HomePage');
-});*/
+app.get("/", (req, res) => {
+  res.send("Hello from HomePage");
+});
 
-app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on port: http://localhost:${PORT}`)
+);
 
-app.get('/',function(req,res){
-   fs.readFile(__dirname + '/View/eksamensopg.html', 'utf8', function(err, text){
-        res.send(text);
-   })
-})
+// app.get("/", function (req, res) {
+//   fs.readFile("/View/eksamensopg.html", "utf8", function (err, text) {
+//     res.send(text);
+//   });
+// });
